d7ae66b677aabfe1ed44c0c82a8b00dfcf2bb9e5
Novan R. Kuncoro <enerka4ig@gmail.com>
2022-04-08 20:34:26 +0700

    [UI] Add transparent background to navigation bar on scrolling the page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import cx from "clsx"
 import { useTranslation } from "next-i18next"
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 import LocaleSwitcher from "./LocaleSwitcher"
 
 const links = [
@@ -11,8 +12,25 @@ const links = [
 ]
 
 export default function Navbar() {
+  const [scrollY, setScrollY] = useState()
+
+  const handleWindowScroll = () => {
+    setScrollY(window.scrollY)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleWindowScroll)
+    }
+  }, [])
+
+  const navClass = cx([
+    'fixed top-0 z-50 w-full p-8 transition-colors text-stone-100',
+    scrollY > 100 ? 'bg-stone-800/75 backdrop-blur-lg' : 'bg-transparent'
+  ])
+
   return (
-    <nav className="sticky top-0 z-50 w-full p-8 bg-stone-800 text-stone-100">
+    <nav className={navClass}>
       <div className="container flex items-center justify-between mx-auto">
         <span className="font-bold tracking-tight">- Nayo Homestay -</span>
         <span className="hidden md:flex items-center gap-8">
